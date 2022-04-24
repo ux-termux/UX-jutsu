f11a2d33bee36cc1881f9860bfb9ce428d60214d
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-04-24 18:59:54 +0530

    Update client.py

--- userge/core/client.py
+++ userge/core/client.py
@@ -135,7 +135,7 @@ class Userge(_AbstractUserge):
         if Config.HU_STRING_SESSION and Config.BOT_TOKEN:
             RawClient.DUAL_MODE = True
             kwargs['bot'] = UsergeBot(bot=self, **kwargs)
-        kwargs['name'] = Config.HU_STRING_SESSION(name='session_string', **kwargs) or "in_memory=True"
+        kwargs['session_string'] = Config.HU_STRING_SESSION or "in_memory=True"
         super().__init__(**kwargs)
         self.executor.shutdown()
         self.executor = pool._get()  # pylint: disable=protected-access
