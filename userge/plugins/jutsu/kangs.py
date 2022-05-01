2628f079af9958a2096f523e981dd6a7cee11eec
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-05-01 19:34:24 +0530

    Update kangs.py

--- userge/plugins/jutsu/kangs.py
+++ userge/plugins/jutsu/kangs.py
@@ -197,19 +197,15 @@ async def kang_(message: Message):
                 await conv.get_response(mark_read=True)
                 await conv.send_message(packname)
                 msg = await conv.get_response(mark_read=True)
-                if is_video : await conv.send_message(packname)
-                msg = await conv.get_response(mark_read=True)
-                limit = "50" if is_video else "120"
+                limit = "50" if (is_video or is_anim) else "120"
                 while limit in msg.text:
                     pack += 1
                     packname = f"a{user.id}_by_userge_{pack}"
                     packnick = f"{custom_packnick} Vol.{pack}"
                     if is_anim:
-                        pack += 1
                         packname += f"_anim{pack}"
                         packnick += f" (Animated){pack}"
                     if is_video:
-                        pack += 1
                         packname += f"_video{pack}"
                         packnick += f" (Video){pack}"
                     await kang_msg.edit(
